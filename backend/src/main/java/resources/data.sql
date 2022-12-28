f46660a4ef05415da503cb1b6216d4c76a729a70
Elvira Mjeshtri <elviramjeshtri@yahoo.com>
2022-12-28 11:08:44 +0100

    personality-test | backend | Configure in memory database h2 and add JPA dependency

---
+++ backend/src/main/java/resources/data.sql
@@ -0,0 +1,32 @@
+INSERT INTO questions (questionId, question) VALUES (1, 'You’re really busy at work and a colleague is telling you their life story and personal woes. You');
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (1, 1, 'Don’t dare to interrupt them', 1);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (2, 1, 'Think it’s more important to give them some of your time; work can wait',2);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (3, 1, 'Listen, but with only with half an ear', 3);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (4, 1, 'Interrupt and explain that you are really busy at the moment', 4);
+
+
+INSERT INTO questions (questionId, question) VALUES (2, 'You’ve been sitting in the doctor’s waiting room for more than 25 minutes. You:');
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (1, 2, 'Look at your watch every two minutes', 1);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (2, 2, 'Bubble with inner anger, but keep quiet',2);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (3, 2, 'Explain to other equally impatient people in the room that the doctor is always running late', 3);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (4, 2, 'Complain in a loud voice, while tapping your foot impatiently', 4);
+
+
+INSERT INTO questions (questionId, question) VALUES (3, 'You’re having an animated discussion with a colleague regarding a project that you’re in charge of. You:');
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (1, 3, 'Don’t dare contradict them', 1);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (2, 3, 'Think that they are obviously right',2);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (3, 3, 'Defend your own point of view, tooth and nail', 3);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (4, 3, 'Continuously interrupt your colleague', 4);
+
+INSERT INTO questions (questionId, question) VALUES (4, 'You are taking part in a guided tour of a museum. You:');
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (1, 4, 'Are a bit too far towards the back so don’t really hear what the guide is saying', 1);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (2, 4, 'Follow the group without question',2);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (3, 4, 'Make sure that everyone is able to hear properly', 3);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (4, 4, 'Are right up the front, adding your own comments in a loud voice', 4);
+
+
+INSERT INTO questions (questionId, question) VALUES (5, 'During dinner parties at your home, you have a hard time with people who:');
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (1, 5, 'Ask you to tell a story in front of everyone else', 1);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (2, 5, 'Talk privately between themselves',2);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (3, 5, 'Hang around you all evening', 3);
+INSERT INTO answers (answerId, questionId, answer, precedence, points) VALUES (4, 5, 'Always drag the conversation back to themselves', 4);
