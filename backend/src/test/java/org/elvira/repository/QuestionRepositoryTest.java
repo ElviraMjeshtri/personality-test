dc913a4cd3ab787dc5a661f2ad709564b94b48ca
Elvira Mjeshtri <elviramjeshtri@yahoo.com>
2023-01-04 14:55:45 +0100

    personality-test | backend | Add JPaTest for findAll method repository

---
+++ backend/src/test/java/org/elvira/repository/QuestionRepositoryTest.java
@@ -0,0 +1,28 @@
+package org.elvira.repository;
+
+import org.elvira.entity.Question;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class QuestionRepositoryTest {
+
+    @Autowired
+    QuestionRepository questionRepository;
+
+    @Test
+    public void testFindAll(){
+        Pageable paging = PageRequest.of(1, 1);
+        Page<Question> pageQuestions = questionRepository.findAll(paging);
+        List<Question> questions = pageQuestions.getContent();
+        assertThat(questions).hasSize(1);
+    }
+}
