d2155de6ab0e5b8f2118a4d719670cd1c66c91a7
emj <elvira@codefriends.ch>
2023-01-06 00:55:30 +0100

    personality-test | frontend|Create QuestionComponent.vue to display question with answer options

--- frontend/src/component/QuestionComponent.vue
+++ frontend/src/component/QuestionComponent.vue
@@ -1,10 +1,70 @@
 <template>
-
+  <section>
+    <div class="quiz-info">
+      <span class="question">{{ question.question }} </span>
+    </div>
+    <div class="options">
+      <label v-for="(option, index) in question.answerList" key="index">
+        <input type="radio"
+               :id="index"
+               :name="question.questionId"
+               :value="option.points"
+               @change="addAnswerToList($event,currentQuestionIndex)">
+        <span>{{ option.answer }}</span>
+      </label>
+    </div>
+    <button v-if="isTestCompleted" @click="showResults">Show Results</button>
+    <button v-else @click="goToNextQuestion">Next Question</button>
+  </section>
 </template>
 
 <script>
+import {useStore} from "vuex";
+import {computed, ref} from "vue";
+
 export default {
-  name: "QuestionComponent"
+  name: "QuestionComponent",
+  props: {
+    question: Object,
+    listSize: Number
+  },
+  setup(props) {
+    const store = useStore()
+    let selectedAnswers = computed(() => store.state.selectedAnswers);
+    let isTestCompleted = computed(() => store.state.isTestCompleted)
+    let currentQuestionIndex = computed(() => store.state.currentQuestionIndex)
+    function addAnswerToList (e, index){
+      const answer = selectedAnswers.value[index];
+      let points = e.target.value
+      if(answer === undefined){
+        selectedAnswers.value.push(points)
+      }else {
+        selectedAnswers.value.splice(index, 1)
+        selectedAnswers.value.push(points)
+      }
+    }
+    function goToNextQuestion() {
+      if (currentQuestionIndex.value < props.listSize - 1) {
+        const nextQuestionIndex = currentQuestionIndex.value + 1
+        store.commit('fetchCurrentQuestionIndex', nextQuestionIndex)
+        return
+      }
+      if(currentQuestionIndex.value === props.listSize - 1){
+        store.commit('fetchIsTestCompleted', true)
+      }
+    }
+
+    function showResults() {
+
+    }
+
+    return {
+      addAnswerToList,
+      goToNextQuestion,
+      showResults,
+      isTestCompleted
+    }
+  }
 }
 </script>
 
--- frontend/src/views/Quiz.vue
+++ frontend/src/views/Quiz.vue
@@ -1,10 +1,41 @@
 <template>
+  <span class="user-area">
+    <QuestionComponent :question="getCurrentQuestion"
+                       :list-size="getQuestionListSize"
+    ></QuestionComponent>
+  </span>
 
 </template>
 
 <script>
+import {ref, computed} from 'vue'
+import {useStore} from 'vuex'
+import QuestionComponent from "@/component/QuestionComponent.vue";
+
 export default {
-  name: "Questions"
+  name: 'Quiz',
+  components: {QuestionComponent},
+  setup() {
+    const store = useStore()
+    store.dispatch('fetchQuestionList')
+    const questionList = computed(() => store.state.questionDataList);
+    let currentQuestionIndex = computed(() => store.state.currentQuestionIndex)
+
+    const getCurrentQuestion = computed(() => {
+      let question = questionList.value.questions[currentQuestionIndex.value];
+      question.index = currentQuestionIndex.value
+      return question
+    });
+
+    const getQuestionListSize = computed(() => {
+      return questionList.value.questions.length
+    });
+
+    return {
+      getCurrentQuestion,
+      getQuestionListSize
+    }
+  }
 }
 </script>
 
