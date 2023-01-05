0e415bc7193f20707a215ab0c3430c1c89a05617
emj <elvira@codefriends.ch>
2023-01-06 00:51:00 +0100

    personality-test | frontend| Add proxy for the apis

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const PROXY = 'http://localhost:8082';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server:{
+    port:4000,
+    proxy:{
+      '^/api': {
+        target: 'http://localhost:8082',
+        changeOrigin: true,
+      }
     }
   }
 })
